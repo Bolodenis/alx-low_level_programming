ef4e4e72235318f7580fd91898b34e6c3f38a0d0
Denis bollo <bollodenis@gmail.com>
2022-06-30 15:22:00 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,4 @@
 #!/bin/bash
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+puts("\"Programming is like building a multilingual puzzle");
 
-int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-
-
-			return (0);
-
-}
+return (0);
