26d58db1e3b9899415f28c7d90f33aeff4d1ed38
Denis bollo <bollodenis@gmail.com>
2022-07-03 23:36:04 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always (Success)
+*/
+int main(void)
+{
+int n;
+for (n = 0; n <= 10; n++)
+	putchar('\n');
+return (0);
+}
+
