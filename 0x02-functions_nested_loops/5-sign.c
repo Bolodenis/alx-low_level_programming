d80e3eb6f9fe45ecac03cf3b518f0c8846524f7a
Denis bollo <bollodenis@gmail.com>
2022-07-06 09:52:07 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
 *print_sign -prints the sign of a number.
 *@n:The number of which the sign will be printed.
@@ -13,7 +13,7 @@ if (n > 0)
 	_putchar('+');
 return (1);
 }
-else if (n == 0)
+else if (n ==+ 0)
 {
 	_putchar('0');
 	return (0);
