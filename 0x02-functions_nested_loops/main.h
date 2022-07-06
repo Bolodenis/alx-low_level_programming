fed28ab3d7a02f74124bafc25f51db9b17dfaf31
Denis bollo <bollodenis@gmail.com>
2022-07-06 11:02:30 -0700

    0.-

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,7 @@
-#ifndef HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
-#define HEADER_FILE
-
-
-
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
