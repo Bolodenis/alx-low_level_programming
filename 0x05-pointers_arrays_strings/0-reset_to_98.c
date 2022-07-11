29b1e0dd7209be108a3c5d81938377dff8800a7c
Denis bollo <bollodenis@gmail.com>
2022-07-11 10:17:32 -0700

    1. Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
+
 /**
 *reset_to_98 - takes a pointer to a parameter and updates its value
 *as parameter and updates
 *Return: no return.
 */
-
 void reset_to_98(int *n)
 {
-int x
 *n = 98;
 }
