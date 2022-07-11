e336819e0d854c898616e8bb20acf5737d252741
Denis bollo <bollodenis@gmail.com>
2022-07-11 11:27:31 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*_strlen -function that returns the length of a string
+*@s: string pointer.
+*Return: returns the length of a string
+*/
+int _strlen(char *s);
+int len;
+for (len = 0 s[len] != '\10'; len++)
+return (len);
