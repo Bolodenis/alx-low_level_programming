2be399ca6d59dee532a41e879d36e99ba5adfada
Denis bollo <bollodenis@gmail.com>
2022-07-11 12:39:38 -0700

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-*_puts -Write a function that prints a string, followed by a new line
-* to stdout
-*@str: this is my input string
+* _puts - Write a function that prints a string, followed by a new line, to
+*stdout.
+*@str: This is my input string
 */
-void _puts(char *str);
+void _puts(char *str)
 {
 int index;
 for (index = 0; str[index] != '\0'; index++)
