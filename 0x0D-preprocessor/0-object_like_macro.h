dfa83460850f98c1e459b5d9f3bb703247826c09
Bolo Denis <bollodenis@gmail.com>
2023-08-14 07:19:57 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * *Main- a header file that defines a macro named SIZE
+ *  as an abbreviation for the token 1024
+ */
+#ifndef MACRO
+#define MACRO
+
+#define SIZE (1024)
+
+#endif
