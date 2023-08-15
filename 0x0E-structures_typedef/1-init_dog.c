808b9f8cc51b40a550d81d4f7ea5b43cc32fc992
Bolo Denis <bollodenis@gmail.com>
2023-08-15 06:43:37 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * function that initialize a variable of type struct dog
+ * @d -struct name
+ * @name- name of the dog
+ * owner- owner of the dog
+ * Author- Denis Bollo
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
