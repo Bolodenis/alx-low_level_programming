4b4997d886bf03742e95dd8b64936c8f066627e5
Bolo Denis <bollodenis@gmail.com>
2023-08-17 05:45:14 -0700

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef PROTYPE_VA_
+#define PROTYPE_VA
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int putchar (int character);
+
+
+#endif
