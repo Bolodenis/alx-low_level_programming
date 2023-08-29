249f479a50e437915750802f7bb627098069a22b
Bolo Denis <bollodenis@gmail.com>
2023-08-29 04:30:31 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,17 +5,14 @@
  * @hed: pointer to the address of the next in thr list
  * Return: void(0)
  */
-
 void free_listint(listint_t *head)
 {
-        listint_t *temp = NULL;
+	listint_t *temp;
 
-        if (head != NULL)
-        {
-                temp = head;
-                head = head->next;
-
-                free(temp);
-        }
-
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }
