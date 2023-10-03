0de1e2072dfdf743c92493f6788718a0cba60ec8
Bolodenis <bollodenis@gmail.com>
2023-10-02 22:34:07 -0700

    0x15-file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,13 @@
-#ifndef MAIN_H
+#ifdef MAIN_H
 #define MAIN_H
 
+
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stddef.h>
 
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
