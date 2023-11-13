66d3ccbad25792e17457b2079faba47ed12fc9a6
Bolodenis <bollodenis@gmail.com>
2023-11-13 11:19:13 -0800

    cpython

---
+++ 0x18-dynamic_libraries/100_main.py
@@ -0,0 +1,11 @@
+import random
+import ctypes
+
+mylib = ctypes.CDLL('./100-operations.so')
+a = random.randint(-100, 100)
+b = random.randint(-100, 100)
+print("{} + {} = {}".format(a, b, cops.add(a, b)))
+print("{} - {} = {}".format(a, b, cops.sub(a, b)))
+print("{} x {} = {}".format(a, b, cops.mul(a, b)))
+print("{} / {} = {}".format(a, b, cops.div(a, b)))
+print("{} % {} = {}".format(a, b, cops.mod(a, b)))
