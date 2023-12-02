764f59b69a3394761877369010a71a0661662ebf
Bolodenis <bollodenis@gmail.com>
2023-12-01 23:10:06 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+
 /**
-*Mainn- entry point
-*Description: 'A new project abot using puts with special symbols'
-*Return: Always 1 (success)
-*/
+ * main - Entry point for the program
+ *
+ * Description: This program writes a specific message to the standard error
+ *              stream using the write system call.
+ *
+ * Return: Always 1 (success)
+ */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
